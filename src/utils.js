fc1bf3fc22e9fd64812ab6bd03a0c88044c9375e
Wolfgang Kluge <wolfgang@klugesoftware.de>
2011-08-30 15:33:48 +0200

    Use internal Array.indexOf() function if available
    Only if not available, create an own version of the indexOf-function.
    It's used only inside contains() (utils.js).

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,36 @@
-function contains(array, value) {
-  /*
-   * Stupid IE does not have Array.prototype.indexOf, otherwise this function
-   * would be a one-liner.
-   */
-  var length = array.length;
-  for (var i = 0; i < length; i++) {
-    if (array[i] === value) {
-      return true;
+// see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf for details
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+    "use strict";
+    if (this === void 0 || this === null) {
+      throw new TypeError();
     }
-  }
-  return false;
+    /*jshint bitwise: false, newcap: false*/
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (len === 0) {
+      return -1;
+    }
+    var n = 0;
+    if (arguments.length > 0) {
+      n = Number(arguments[1]);
+      if (n !== n) { // shortcut for verifying if it's NaN
+        n = 0;
+      } else if (n !== 0 && n !== (1 / 0) && n !== -(1 / 0)) {
+        n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+    }
+    if (n >= len) {
+      return -1;
+    }
+    var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+    for (; k < len; k++) {
+      if (k in t && t[k] === searchElement) {
+        return k;
+      }
+    }
+    return -1;
+  };
 }
 
 // see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach for details
@@ -145,6 +166,10 @@ if (!Array.prototype.map) {
     // 9. return A
     return A;
   };      
+}
+
+function contains(array, value) {
+  return array.indexOf(value) !== -1;
 }
 
 /*
