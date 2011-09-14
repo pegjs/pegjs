cc4910d9b98d00d1beaea114ca9711b9eef70543
David Majda <david@majda.cz>
2011-09-14 12:13:00 +0200

    src/compiler.js: Fix variable redefinition
    Fixes the following JSHint error:
    
      ./src/compiler.js: line 23, col 16, 'i' is already defined.

--- src/compiler.js
+++ src/compiler.js
@@ -16,11 +16,13 @@ PEG.compiler = {
       "stackDepths"
     ];
 
-    for (var i = 0; i < CHECK_NAMES.length; i++) {
+    var i;
+
+    for (i = 0; i < CHECK_NAMES.length; i++) {
       this.checks[CHECK_NAMES[i]](ast);
     }
 
-    for (var i = 0; i < PASS_NAMES.length; i++) {
+    for (i = 0; i < PASS_NAMES.length; i++) {
       ast = this.passes[PASS_NAMES[i]](ast);
     }
 
