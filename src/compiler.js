cd5490dee4d507e0a95d87c04c1550902709a19c
David Majda <david@majda.cz>
2011-10-03 14:06:49 +0200

    Make pass list customizable via |PEG.compiler.appliedPassNames| property

--- src/compiler.js
+++ src/compiler.js
@@ -1,20 +1,24 @@
 PEG.compiler = {
   /*
+   * Names of passes that will get run during the compilation (in the specified
+   * order).
+   */
+  appliedPassNames: [
+    "reportMissingRules",
+    "reportLeftRecursion",
+    "removeProxyRules",
+    "computeStackDepths"
+  ],
+
+  /*
    * Generates a parser from a specified grammar AST. Throws |PEG.GrammarError|
    * if the AST contains a semantic error. Note that not all errors are detected
    * during the generation and some may protrude to the generated parser and
    * cause its malfunction.
    */
   compile: function(ast) {
-    var PASS_NAMES = [
-      "reportMissingRules",
-      "reportLeftRecursion",
-      "removeProxyRules",
-      "computeStackDepths"
-    ];
-
-    for (var i = 0; i < PASS_NAMES.length; i++) {
-      this.passes[PASS_NAMES[i]](ast);
+    for (var i = 0; i < this.appliedPassNames.length; i++) {
+      this.passes[this.appliedPassNames[i]](ast);
     }
 
     var source = this.emitter(ast);
