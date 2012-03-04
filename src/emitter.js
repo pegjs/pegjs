pegjs/pegjs

commit e9d8dc8ebaeda081a6617bb261733bf1a18f2152

More responsibility for building an error message into |SyntaxError|

src/emitter.js
@@ -344,38 +344,19 @@ PEG.compiler.emitter = function(ast) {
             '        #block definition',
             '        ',
             '      #end',
-            '      function buildErrorMessage() {',
-            '        function buildExpected(failuresExpected) {',
-            '          failuresExpected.sort();',
-            '          ',
-            '          var lastFailure = null;',
-            '          var failuresExpectedUnique = [];',
-            '          for (var i = 0; i < failuresExpected.length; i++) {',
-            '            if (failuresExpected[i] !== lastFailure) {',
-            '              failuresExpectedUnique.push(failuresExpected[i]);',
-            '              lastFailure = failuresExpected[i];',
-            '            }',
-            '          }',
-            '          ',
-            '          switch (failuresExpectedUnique.length) {',
-            '            case 0:',
-            '              return "end of input";',
-            '            case 1:',
-            '              return failuresExpectedUnique[0];',
-            '            default:',
-            '              return failuresExpectedUnique.slice(0, failuresExpectedUnique.length - 1).join(", ")',
-            '                + " or "',
-            '                + failuresExpectedUnique[failuresExpectedUnique.length - 1];',
+            '      ',
+            '      function cleanupExpected(expected) {',
+            '        expected.sort();',
+            '        ',
+            '        var lastExpected = null;',
+            '        var cleanExpected = [];',
+            '        for (var i = 0; i < expected.length; i++) {',
+            '          if (expected[i] !== lastExpected) {',
+            '            cleanExpected.push(expected[i]);',
+            '            lastExpected = expected[i];',
             '          }',
             '        }',
-            '        ',
-            '        var expected = buildExpected(rightmostFailuresExpected);',
-            '        var actualPos = Math.max(pos, rightmostFailuresPos);',
-            '        var actual = actualPos < input.length',
-            '          ? quote(input.charAt(actualPos))',
-            '          : "end of input";',
-            '        ',
-            '        return "Expected " + expected + " but " + actual + " found.";',
+            '        return cleanExpected;',
             '      }',
             '      ',
             '      function computeErrorPosition() {',
@@ -440,10 +421,14 @@ PEG.compiler.emitter = function(ast) {
             '       * handle these states.',
             '       */',
             '      if (result === null || pos !== input.length) {',
+            '        var offset = Math.max(pos, rightmostFailuresPos);',
+            '        var found = offset < input.length ? input.charAt(offset) : null;',
             '        var errorPosition = computeErrorPosition();',
+            '        ',
             '        throw new this.SyntaxError(',
-            '          buildErrorMessage(),',
-            '          Math.max(pos, rightmostFailuresPos),',
+            '          cleanupExpected(rightmostFailuresExpected),',
+            '          found,',
+            '          offset,',
             '          errorPosition.line,',
             '          errorPosition.column',
             '        );',
@@ -458,9 +443,30 @@ PEG.compiler.emitter = function(ast) {
             '  ',
             '  /* Thrown when a parser encounters a syntax error. */',
             '  ',
-            '  result.SyntaxError = function(message, offset, line, column) {',
+            '  result.SyntaxError = function(expected, found, offset, line, column) {',
+            '    function buildMessage(expected, found) {',
+            '      var expectedHumanized, foundHumanized;',
+            '      ',
+            '      switch (expected.length) {',
+            '        case 0:',
+            '          expectedHumanized = "end of input";',
+            '          break;',
+            '        case 1:',
+            '          expectedHumanized = expected[0];',
+            '          break;',
+            '        default:',
+            '          expectedHumanized = expected.slice(0, expected.length - 1).join(", ")',
+            '            + " or "',
+            '            + expected[expected.length - 1];',
+            '      }',
+            '      ',
+            '      foundHumanized = found ? quote(found) : "end of input";',
+            '      ',
+            '      return "Expected " + expectedHumanized + " but " + foundHumanized + " found.";',
+            '    }',
+            '    ',
             '    this.name = "SyntaxError";',
-            '    this.message = message;',
+            '    this.message = buildMessage(expected, found);',
             '    this.offset = offset;',
             '    this.line = line;',
             '    this.column = column;',

src/parser.js
@@ -3502,38 +3502,19 @@ PEG.parser = (function(){
         return result0;
       }
       
-      function buildErrorMessage() {
-        function buildExpected(failuresExpected) {
-          failuresExpected.sort();
-          
-          var lastFailure = null;
-          var failuresExpectedUnique = [];
-          for (var i = 0; i < failuresExpected.length; i++) {
-            if (failuresExpected[i] !== lastFailure) {
-              failuresExpectedUnique.push(failuresExpected[i]);
-              lastFailure = failuresExpected[i];
-            }
-          }
-          
-          switch (failuresExpectedUnique.length) {
-            case 0:
-              return "end of input";
-            case 1:
-              return failuresExpectedUnique[0];
-            default:
-              return failuresExpectedUnique.slice(0, failuresExpectedUnique.length - 1).join(", ")
-                + " or "
-                + failuresExpectedUnique[failuresExpectedUnique.length - 1];
+      
+      function cleanupExpected(expected) {
+        expected.sort();
+        
+        var lastExpected = null;
+        var cleanExpected = [];
+        for (var i = 0; i < expected.length; i++) {
+          if (expected[i] !== lastExpected) {
+            cleanExpected.push(expected[i]);
+            lastExpected = expected[i];
           }
         }
-        
-        var expected = buildExpected(rightmostFailuresExpected);
-        var actualPos = Math.max(pos, rightmostFailuresPos);
-        var actual = actualPos < input.length
-          ? quote(input.charAt(actualPos))
-          : "end of input";
-        
-        return "Expected " + expected + " but " + actual + " found.";
+        return cleanExpected;
       }
       
       function computeErrorPosition() {
@@ -3595,10 +3576,14 @@ PEG.parser = (function(){
        * handle these states.
        */
       if (result === null || pos !== input.length) {
+        var offset = Math.max(pos, rightmostFailuresPos);
+        var found = offset < input.length ? input.charAt(offset) : null;
         var errorPosition = computeErrorPosition();
+        
         throw new this.SyntaxError(
-          buildErrorMessage(),
-          Math.max(pos, rightmostFailuresPos),
+          cleanupExpected(rightmostFailuresExpected),
+          found,
+          offset,
           errorPosition.line,
           errorPosition.column
         );
@@ -3613,9 +3598,30 @@ PEG.parser = (function(){
   
   /* Thrown when a parser encounters a syntax error. */
   
-  result.SyntaxError = function(message, offset, line, column) {
+  result.SyntaxError = function(expected, found, offset, line, column) {
+    function buildMessage(expected, found) {
+      var expectedHumanized, foundHumanized;
+      
+      switch (expected.length) {
+        case 0:
+          expectedHumanized = "end of input";
+          break;
+        case 1:
+          expectedHumanized = expected[0];
+          break;
+        default:
+          expectedHumanized = expected.slice(0, expected.length - 1).join(", ")
+            + " or "
+            + expected[expected.length - 1];
+      }
+      
+      foundHumanized = found ? quote(found) : "end of input";
+      
+      return "Expected " + expectedHumanized + " but " + foundHumanized + " found.";
+    }
+    
     this.name = "SyntaxError";
-    this.message = message;
+    this.message = buildMessage(expected, found);
     this.offset = offset;
     this.line = line;
     this.column = column;

commit ed45a1808efd90bcf52332b36ff231d44f17269d

Fix |quote| visibility in generated parsers
|quote| is used outside of the |parse| function so it must be defined in
more outer scope.

Fixes a problem (introduced in e9d8dc8ebaeda081a6617bb261733bf1a18f2152)
where construction of some error messages could throw an error.

src/emitter.js
@@ -248,6 +248,30 @@ PEG.compiler.emitter = function(ast) {
             '(function(){',
             '  /* Generated by PEG.js @VERSION (http://pegjs.majda.cz/). */',
             '  ',
+            /* This needs to be in sync with |quote| in utils.js. */
+            '  function quote(s) {',
+            '    /*',
+            '     * ECMA-262, 5th ed., 7.8.4: All characters may appear literally in a',
+            '     * string literal except for the closing quote character, backslash,',
+            '     * carriage return, line separator, paragraph separator, and line feed.',
+            '     * Any character may appear in the form of an escape sequence.',
+            '     *',
+            '     * For portability, we also escape escape all control and non-ASCII',
+            '     * characters. Note that "\\0" and "\\v" escape sequences are not used',
+            '     * because JSHint does not like the first and IE the second.',
+            '     */',
+            '     return \'"\' + s',
+            '      .replace(/\\\\/g, \'\\\\\\\\\')  // backslash',
+            '      .replace(/"/g, \'\\\\"\')    // closing quote character',
+            '      .replace(/\\x08/g, \'\\\\b\') // backspace',
+            '      .replace(/\\t/g, \'\\\\t\')   // horizontal tab',
+            '      .replace(/\\n/g, \'\\\\n\')   // line feed',
+            '      .replace(/\\f/g, \'\\\\f\')   // form feed',
+            '      .replace(/\\r/g, \'\\\\r\')   // carriage return',
+            '      .replace(/[\\x00-\\x07\\x0B\\x0E-\\x1F\\x80-\\uFFFF]/g, escape)',
+            '      + \'"\';',
+            '  }',
+            '  ',
             '  var result = {',
             '    /*',
             '     * Parses the input with a generated parser. If the parsing is successfull,',
@@ -301,30 +325,6 @@ PEG.compiler.emitter = function(ast) {
             '        }',
             '        ',
             '        return \'\\\\\' + escapeChar + padLeft(charCode.toString(16).toUpperCase(), \'0\', length);',
-            '      }',
-            '      ',
-            /* This needs to be in sync with |quote| in utils.js. */
-            '      function quote(s) {',
-            '        /*',
-            '         * ECMA-262, 5th ed., 7.8.4: All characters may appear literally in a',
-            '         * string literal except for the closing quote character, backslash,',
-            '         * carriage return, line separator, paragraph separator, and line feed.',
-            '         * Any character may appear in the form of an escape sequence.',
-            '         *',
-            '         * For portability, we also escape escape all control and non-ASCII',
-            '         * characters. Note that "\\0" and "\\v" escape sequences are not used',
-            '         * because JSHint does not like the first and IE the second.',
-            '         */',
-            '        return \'"\' + s',
-            '          .replace(/\\\\/g, \'\\\\\\\\\')  // backslash',
-            '          .replace(/"/g, \'\\\\"\')    // closing quote character',
-            '          .replace(/\\x08/g, \'\\\\b\') // backspace',
-            '          .replace(/\\t/g, \'\\\\t\')   // horizontal tab',
-            '          .replace(/\\n/g, \'\\\\n\')   // line feed',
-            '          .replace(/\\f/g, \'\\\\f\')   // form feed',
-            '          .replace(/\\r/g, \'\\\\r\')   // carriage return',
-            '          .replace(/[\\x00-\\x07\\x0B\\x0E-\\x1F\\x80-\\uFFFF]/g, escape)',
-            '          + \'"\';',
             '      }',
             '      ',
             '      function matchFailed(failure) {',

src/parser.js
@@ -1,6 +1,29 @@
 PEG.parser = (function(){
   /* Generated by PEG.js 0.6.2 (http://pegjs.majda.cz/). */
   
+  function quote(s) {
+    /*
+     * ECMA-262, 5th ed., 7.8.4: All characters may appear literally in a
+     * string literal except for the closing quote character, backslash,
+     * carriage return, line separator, paragraph separator, and line feed.
+     * Any character may appear in the form of an escape sequence.
+     *
+     * For portability, we also escape escape all control and non-ASCII
+     * characters. Note that "\0" and "\v" escape sequences are not used
+     * because JSHint does not like the first and IE the second.
+     */
+     return '"' + s
+      .replace(/\\/g, '\\\\')  // backslash
+      .replace(/"/g, '\\"')    // closing quote character
+      .replace(/\x08/g, '\\b') // backspace
+      .replace(/\t/g, '\\t')   // horizontal tab
+      .replace(/\n/g, '\\n')   // line feed
+      .replace(/\f/g, '\\f')   // form feed
+      .replace(/\r/g, '\\r')   // carriage return
+      .replace(/[\x00-\x07\x0B\x0E-\x1F\x80-\uFFFF]/g, escape)
+      + '"';
+  }
+  
   var result = {
     /*
      * Parses the input with a generated parser. If the parsing is successfull,
@@ -107,29 +130,6 @@ PEG.parser = (function(){
         }
         
         return '\\' + escapeChar + padLeft(charCode.toString(16).toUpperCase(), '0', length);
-      }
-      
-      function quote(s) {
-        /*
-         * ECMA-262, 5th ed., 7.8.4: All characters may appear literally in a
-         * string literal except for the closing quote character, backslash,
-         * carriage return, line separator, paragraph separator, and line feed.
-         * Any character may appear in the form of an escape sequence.
-         *
-         * For portability, we also escape escape all control and non-ASCII
-         * characters. Note that "\0" and "\v" escape sequences are not used
-         * because JSHint does not like the first and IE the second.
-         */
-        return '"' + s
-          .replace(/\\/g, '\\\\')  // backslash
-          .replace(/"/g, '\\"')    // closing quote character
-          .replace(/\x08/g, '\\b') // backspace
-          .replace(/\t/g, '\\t')   // horizontal tab
-          .replace(/\n/g, '\\n')   // line feed
-          .replace(/\f/g, '\\f')   // form feed
-          .replace(/\r/g, '\\r')   // carriage return
-          .replace(/[\x00-\x07\x0B\x0E-\x1F\x80-\uFFFF]/g, escape)
-          + '"';
       }
       
       function matchFailed(failure) {