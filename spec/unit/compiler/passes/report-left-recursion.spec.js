21c6378e7a6066d6b712aa4599e4dd4c32367b80
Mingun <alexander_sergey@mail.ru>
2014-11-06 23:14:02 +0500

    Add test for left recursion through template arguments.

--- spec/unit/compiler/passes/report-left-recursion.spec.js
+++ spec/unit/compiler/passes/report-left-recursion.spec.js
@@ -26,4 +26,28 @@ describe("compiler pass |reportLeftRecursion|", function() {
       expect(pass).not.toReportError('start = "a" "b" start');
     });
   });
+
+  describe("in templates", function() {
+    it("reports left recursion for template argument", function() {
+      expect(pass).toReportError('start<A> = start<A> "a" "b"', {
+        message: 'Left recursion detected for rule "start".'
+      });
+      expect(pass).toReportError([
+        'start = template<start> "a"',
+        'template<A> = A "b"',
+      ].join('\n'), {
+        // message: 'Left recursion detected for rule "start" (through template parameter "A" of rule "template").'
+        message: 'Left recursion detected for rule "start".'
+      });
+
+      expect(pass).not.toReportError([
+        'start = template<start> "a"',
+        'template<A> = "b" A',
+      ].join('\n'));
+      expect(pass).not.toReportError([
+        'start = template<"b" start> "a"',
+        'template<A> = A "b"',
+      ].join('\n'));
+    });
+  });
 });
