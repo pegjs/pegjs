8a85600b2f25147dc7a161b864f531af393e464f
Mingun <alexander_sergey@mail.ru>
2014-12-12 22:51:09 +0500

    Correct and imrove testcases.

--- spec/unit/compiler/passes/report-left-recursion.spec.js
+++ spec/unit/compiler/passes/report-left-recursion.spec.js
@@ -2,28 +2,56 @@ describe("compiler pass |reportLeftRecursion|", function() {
   var pass = PEG.compiler.passes.check.reportLeftRecursion;
 
   it("reports direct left recursion", function() {
-    expect(pass).toReportError('start = start', {
-      message: 'Left recursion detected for rule \"start\".'
-    });
+    var tests = [
+      'start = start;',
+      'start = start {};',
+      'start = "a"? start;',
+      'start = "a"* start;',
+      'start = !"a" start;',
+      'start = &"a" start;',
+      'start = !{} start;',
+      'start = &{} start;',
+      'start = "" start;',
+      'start = [] start;'
+    ];
+    for (var i = 0; i < tests.length; ++i) {
+      expect(pass).toReportError(tests[i], {
+        message: 'Left recursion detected: start->start'
+      });
+    }
   });
 
   it("reports indirect left recursion", function() {
-    expect(pass).toReportError([
-      'start = stop',
-      'stop  = start'
-    ].join("\n"), {
-      message: 'Left recursion detected for rule \"start\".'
-    });
+    var tests = [
+      'start = stop; stop = start;',
+      'start = stop; stop = start {};',
+      'start = stop; stop = "a"? start;',
+      'start = stop; stop = "a"* start;',
+      'start = stop; stop = !"a" start;',
+      'start = stop; stop = &"a" start;',
+      'start = stop; stop = !{} start;',
+      'start = stop; stop = &{} start;',
+      'start = stop; stop = "" start;',
+      'start = stop; stop = [] start;'
+    ];
+    for (var i = 0; i < tests.length; ++i) {
+      expect(pass).toReportError(tests[i], {
+        message: 'Left recursion detected: start->stop->start'
+      });
+    }
+  });
+  it("not report reports indirect left recursion", function() {
   });
 
   describe("in sequences", function() {
     it("reports left recursion only for the first element", function() {
       expect(pass).toReportError('start = start "a" "b"', {
-        message: 'Left recursion detected for rule \"start\".'
+        message: 'Left recursion detected: start->start'
       });
 
       expect(pass).not.toReportError('start = "a" start "b"');
       expect(pass).not.toReportError('start = "a" "b" start');
+      expect(pass).not.toReportError('start = "a"+ start');
     });
   });
 });
