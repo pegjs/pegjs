e17889d676c202cdc66febd7f70a43740fc3e3bc
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2015-05-27 13:16:18 +0200

    Added stack property to custom errors

--- spec/behavior/generated-parser-behavior.spec.js
+++ spec/behavior/generated-parser-behavior.spec.js
@@ -1517,6 +1517,18 @@ describe("generated parser behavior", function() {
           });
         });
       });
+
+      describe("error object", function () {
+        it("has stack property", function () {
+          var message = 'some error', err;
+
+          err = new PEG.parser.SyntaxError(message);
+          expect(err.stack).toBeDefined();
+
+          err = new PEG.GrammarError(message);
+          expect(err.stack).toBeDefined();
+        });
+      });
     });
 
     /*
