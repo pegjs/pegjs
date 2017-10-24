3c6523ff836b4e8edc38f2b508eb5f3f5b61ee44
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2017-10-24 19:46:10 +0100

    Switched eslint configuration
    Was using 'eslint-config-dmajda' by @dmajda, but since it's diffucult for me to use multiple styles across multiple projects, I'm switching to my own 'eslint-config-futagozaryuu'.

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+
+    "extends": "futagozaryuu/node-v4",
+    "root": true
+
+};
---
+++ test/.eslintrc.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+
+    "extends": "futagozaryuu/test",
+    "rules": {
+
+        "node/shebang": 0
+
+    }
+
+};
