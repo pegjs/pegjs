881eb67545c2968ab8972cb4a7813ef5df5a80db
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2017-11-29 20:52:07 +0000

    Removed redundant css files
    The css files used in the benchmark contained 2 files for IE that contained syntax errors. These files weren't actually used by the benchmark, but were still in the repository.

--- test/benchmark/benchmarks.js
+++ test/benchmark/benchmarks.js
@@ -16,25 +16,21 @@ const benchmarks = [
         id: "css",
         title: "CSS",
         tests: [
-            { file: "blueprint/src/reset.css", title: "Blueprint - reset.css (source)" },
-            { file: "blueprint/src/typography.css", title: "Blueprint - typography.css (source)" },
+            { file: "960.gs/min/960_24_col.css", title: "960.gs - 960_24_col.css (minified)" },
+            { file: "960.gs/min/960.css", title: "960.gs - 960.css (minified)" },
+            { file: "960.gs/min/reset.css", title: "960.gs - reset.css (minified)" },
+            { file: "960.gs/min/text.css", title: "960.gs - text.css (minified)" },
+            { file: "960.gs/src/960_24_col.css", title: "960.gs - 960_24_col.css (source)" },
+            { file: "960.gs/src/960.css", title: "960.gs - 960.css (source)" },
+            { file: "960.gs/src/reset.css", title: "960.gs - reset.css (source)" },
+            { file: "960.gs/src/text.css", title: "960.gs - text.css (source)" },
+            { file: "blueprint/min/print.css", title: "Blueprint - print.css (minified)" },
+            { file: "blueprint/min/screen.css", title: "Blueprint - screen.css (minified)" },
             { file: "blueprint/src/forms.css", title: "Blueprint - forms.css (source)" },
             { file: "blueprint/src/grid.css", title: "Blueprint - grid.css (source)" },
             { file: "blueprint/src/print.css", title: "Blueprint - print.css (source)" },
-            // Contains syntax errors.
-            // { file: "blueprint/src/ie.css",         title: "Blueprint - ie.css (source)"         },
-            { file: "blueprint/min/screen.css", title: "Blueprint - screen.css (minified)" },
-            { file: "blueprint/min/print.css", title: "Blueprint - print.css (minified)" },
-            // Contains syntax errors.
-            // { file: "blueprint/min/ie.css",         title: "Blueprint - ie.css (minified)"       },
-            { file: "960.gs/src/reset.css", title: "960.gs - reset.css (source)" },
-            { file: "960.gs/src/text.css", title: "960.gs - text.css (source)" },
-            { file: "960.gs/src/960.css", title: "960.gs - 960.css (source)" },
-            { file: "960.gs/src/960_24_col.css", title: "960.gs - 960_24_col.css (source)" },
-            { file: "960.gs/min/reset.css", title: "960.gs - reset.css (minified)" },
-            { file: "960.gs/min/text.css", title: "960.gs - text.css (minified)" },
-            { file: "960.gs/min/960.css", title: "960.gs - 960.css (minified)" },
-            { file: "960.gs/min/960_24_col.css", title: "960.gs - 960_24_col.css (minified)" }
+            { file: "blueprint/src/reset.css", title: "Blueprint - reset.css (source)" },
+            { file: "blueprint/src/typography.css", title: "Blueprint - typography.css (source)" }
         ]
     }
 ];
