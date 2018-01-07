4cc9185a781ad8ff3b902433809ab452d2586b59
Mingun <Alexander_Sergey@mail.ru>
2018-01-07 19:51:06 +0500

    Improve error when reserved word used as label (#552)
    Before this commit error looks like (for input `start = break:'a'`)
    
    > Expected "!", "$", "&", "(", "*", "+", ".", "/", "/*", "//", ";", "?", character class, code block, comment, end of line, identifier, literal, or whitespace but ":" found.
    
    After this error looks like
    
    > Label can't be a reserved word "break".

--- test/spec/api/pegjs-api.spec.js
+++ test/spec/api/pegjs-api.spec.js
@@ -308,6 +308,91 @@ describe( "PEG.js API", function () {
 
         // The |plugins| option is tested in plugin API tests.
 
+        describe( "reserved words", function () {
+
+            const RESERVED_WORDS = [
+                "break",
+                "case",
+                "catch",
+                "class",
+                "const",
+                "continue",
+                "debugger",
+                "default",
+                "delete",
+                "do",
+                "else",
+                "enum",
+                "export",
+                "extends",
+                "false",
+                "finally",
+                "for",
+                "function",
+                "if",
+                "import",
+                "instanceof",
+                "in",
+                "new",
+                "null",
+                "return",
+                "super",
+                "switch",
+                "this",
+                "throw",
+                "true",
+                "try",
+                "typeof",
+                "var",
+                "void",
+                "while",
+                "with"
+            ];
+
+            describe( "throws an exception on reserved JS words used as labels", function () {
+
+                for ( const label of RESERVED_WORDS ) {
+
+                    it( label, function () {
+
+                        expect( () => {
+
+                            peg.generate( [
+                                "start = " + label + ":end",
+                                "end = 'a'"
+                            ].join( "\n" ), { output: "source" } );
+
+                        } ).to.throw( peg.parser.SyntaxError );
+
+                    } );
+
+                }
+
+            } );
+
+            describe( "not throws an exception on reserved JS words used as rule name", function () {
+
+                for ( const rule of RESERVED_WORDS ) {
+
+                    it( rule, function () {
+
+                        expect( () => {
+
+                            peg.generate( [
+                                "start = " + rule,
+                                rule + " = 'a'"
+                            ].join( "\n" ), { output: "source" } );
+
+                        } ).to.not.throw( peg.parser.SyntaxError );
+
+                    } );
+
+                }
+
+            } );
+
+        } );
+
         it( "accepts custom options", function () {
 
             peg.generate( "start = 'a'", { foo: 42 } );
