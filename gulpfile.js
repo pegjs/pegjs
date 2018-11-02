aa58b73cae0b53b95a19ff5b905b81cca03e0ce5
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-11-02 18:05:53 +0000

    Use ESLint directly instead of gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,7 @@
 
 const { run } = require( "@futagoza/child-process" );
 const { series, src, task } = require( "@futagoza/gulpx" );
-const eslint = require( "gulp-eslint" );
 const del = require( "del" );
-
-// Run ESLint on all JavaScript files.
-task( "lint", () => [
-
-    src( [
-        "**/.*rc.js",
-        "packages/**/*.js",
-        "test/**/*.js",
-        "tools/**/*.js",
-        "src/*.js",
-        "rollup.config.js",
-        "gulpfile.js",
-        "server.js",
-    ] ),
-    eslint( { dotfiles: true } ),
-    eslint.format(),
-    eslint.failAfterError(),
-
-] );
 
 // Generate the grammar parser.
 task( "build:parser", () =>
