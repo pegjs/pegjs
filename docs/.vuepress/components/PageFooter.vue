a8e75345950806fd3f9c866ef8d13b45616ef569
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-12-22 09:11:22 +0000

    Add basic config for Vuepress

---
+++ docs/.vuepress/components/PageFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="footer">
+        Released under the <a href="https://opensource.org/licenses/MIT">MIT License</a> | Copyright © 2017+ Futago-za Ryuu
+    </div>
+</template>
