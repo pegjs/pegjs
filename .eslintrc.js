0074d0456aef0e4c4ab70db680ffcaf64ada9923
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-01-15 19:09:26 +0000

    Upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,6 @@ module.exports = {
 
         "no-eval": 0,
 
-        // Disabled until https://github.com/eslint/eslint/issues/11178 is resolved
-        "padding-line-between-statements": 0,
-
     },
 
 };
